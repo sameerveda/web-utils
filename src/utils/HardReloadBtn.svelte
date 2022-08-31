eb9ed7e0dfce604392c7db36e057eeed0a963088
sameer veda <sameerveda@gmail.com>
2022-08-31 13:56:47 +0530

    init

---
+++ src/utils/HardReloadBtn.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let text = 'hard reset';
+  const action = () => chrome.runtime.reload();
+</script>
+
+<button on:click={action}>{text}</button>
---
+++ src/utils/OpenOptionPageBtn.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let text = 'Option Page';
+  const action = () => chrome.runtime.openOptionsPage();
+</script>
+
+<button on:click={action}>{text}</button>
